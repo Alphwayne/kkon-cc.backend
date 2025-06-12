9f13cce5163712d4652457a11f0571850e254dac
Amosu Daniel <amosudnl896@gmail.com>
2025-06-12 09:59:13 +0100

    updated path

--- srs/Server.js
+++ srs/Server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const checkCoverage = require("./checkCoverage");  // <-- corrected here
+const checkCoverage = require("../checkCoverage");  // <-- corrected here
 
 const app = express();
 
